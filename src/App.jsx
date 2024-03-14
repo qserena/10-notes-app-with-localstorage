a77c218b69994adcb2147ba99b78955d5a906fba
Tom Serenander <tom@serenander.se>
2024-03-14 10:33:31 +0100

    Used the optional chaining operator (?.)

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,7 @@ export default function App() {
 	const [notes, setNotes] = React.useState(
 		() => JSON.parse(localStorage.getItem('notes')) || []
 	)
-	const [currentNoteId, setCurrentNoteId] = React.useState(
-		(notes[0] && notes[0].id) || ''
-	)
+	const [currentNoteId, setCurrentNoteId] = React.useState(notes[0]?.id || '')
 
 	React.useEffect(() => {
 		localStorage.setItem('notes', JSON.stringify(notes))
